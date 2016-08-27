f54793fd085ad446df0946915fee063db774e999
Carlos Rodriguez <carlos@s8f.org>
2016-08-26 20:05:43 -0700

    disable sma

--- core/indicators/sma/_codemap.js
+++ core/indicators/sma/_codemap.js
@@ -1,5 +1,5 @@
 module.exports = {
   _ns: 'zenbrain',
   'reporter_cols.sma': require('./reporter_col'),
-  'tick_reducers[10]': require('./tick_reducer')
+  //'tick_reducers[10]': require('./tick_reducer')
 }
